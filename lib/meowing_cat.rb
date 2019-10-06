f6d2463ec4934131386158b9970a78e6670c9ca5
Meagan Utsey <meagan.utsey@gmail.com>
2019-10-06 20:55:23 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,6 +1,16 @@
 class Cat
+ 
+  def initialize("Meow")
+    @stndout = Meow
+ 
+  end
+ 
+  def name=(Maru)
+    @name = Maru
+  end
+
+class Cat
     attr_writer :Maru
-  attr_accessor :puts
-  #=>"meow"
+  attr_accessor :Meow
  
 end
